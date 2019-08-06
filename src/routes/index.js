66575699d88bdfba24064df408721262a921d7ab
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-08-06 17:09:28 +0300

    handle routes home and books endpoint Relates #7

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router= express.Router();
+const nodeFetch= require('node-fetch');
+
+router.get('/', (req,res) => {
+  res.render('home')
+})
+
+router.get('/books',(req,res) => {
+  nodeFetch(`https://www.googleapis.com/books/v1/volumes?q=${req.query.title}+intitle:${req.query.title}&key=${process.env.API_KEY}`)
+  .then(res=>res.json())
+  .then(response=>res.send(response))
+
+}
+)
+
+module.exports= router;
