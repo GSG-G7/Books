7c03cc62f80828f73082fc010e3412b0a5b9e277
rana <ranasobeid95@gmail.com>
2019-08-07 16:03:25 +0300

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,10 @@ router.get('/books', (req, res) => {
   if(reg.test(req.query.title)){
    nodeFetch(`https://www.googleapis.com/books/v1/volumes?q=${req.query.title}+intitle:${req.query.title}&key=${process.env.API_KEY}`)
       .then(res => res.json())
-      .then(response => res.send(response))
+      .then(response =>{
+        const result =response.items[0];
+        res.render('home',{result})
+      } )
     console.log(req.query.title)
   }else {
     
