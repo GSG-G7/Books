7975b24514d7d4b890706a10957a1cc5d1474d88
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-08-07 16:58:53 +0300

    handle errors Relates #16

--- src/views/helpers/grabData.js
+++ src/views/helpers/grabData.js
@@ -1,4 +1,4 @@
-module.exports= res => res.items?res.items.slice(0,10).map((value)=>{
+module.exports= res => res&&res.items?res.items.slice(0,10).map((value)=>{
             return {title: value.volumeInfo.title,
                    authors: (value.volumeInfo.authors).toString(),
                    description: value.volumeInfo.description,
