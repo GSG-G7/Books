07613586e4372fb152779fa192f87683a504513e
rana <ranasobeid95@gmail.com>
2019-08-08 10:23:42 +0300

    Declare Your reg  Variable as const Relates #22

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/books', (req, res) => {
-   reg=/^[a-zA-Z0-9-\s]*/ ;
+  const reg=/^[a-zA-Z0-9-\s]*/ ;
   if(reg.test(req.query.title) && req.query.title){
    nodeFetch(`https://www.googleapis.com/books/v1/volumes?q=${req.query.title}+intitle:${req.query.title}&key=${process.env.API_KEY}`)
       .then(res => res.json())
